2d2250a56eb62d98be2c0f4f502dfbee543ee01a
Joe Marini <cjmarini68@gmail.com>
2025-04-30 00:39:03 +0000

    Start state

--- Start/Lambdas/BasicLambdas/Program.cs
+++ Start/Lambdas/BasicLambdas/Program.cs
@@ -12,18 +12,11 @@ namespace BasicLambdas
         static void Main(string[] args)
         {
             // Create a basic delegate that squares a number
-            MyDelegate func1 = (x) => x * x;
-            Console.WriteLine("The result of foo is: {0}", func1(5));
 
             // Dynamically change the delegate to something else
-            func1 = (x) => x * 10;
-            Console.WriteLine("The result of bar is: {0}", func1(5));
 
             // Create a delegate that takes multiple arguments
-            MyDelegate2 func2 = (x, y) => {
-                Console.WriteLine("The two-arg lambda: {1}, {0}", x * 10, y);
-            };
-            func2(25, "Some string");
+
         }
     }
 }
