f4e4832b7e7cc53c62a4e5349c43a5aaca2971ae
Tanner Jackley <tanner.jackley@basyspro.com>
2025-06-23 19:01:36 +0000

    lambda

--- Start/Events/BasicEvents/Program.cs
+++ Start/Events/BasicEvents/Program.cs
@@ -6,33 +6,43 @@ namespace BasicEvents
     // define the delegate for the event handler
     public delegate void MyEventHandler(string value);
 
-    class EventPublisher {
+    class EventPublisher
+    {
         private string TheVal = "";
 
         // TODO: declare the event
-
-        public string Val {
-            set {
+        public event MyEventHandler ValueChanged;
+        public string Val
+        {
+            set
+            {
                 this.TheVal = value;
                 // TODO: when the value changes, fire the event
-
+                this.ValueChanged(TheVal);
             }
         }
     }
 
-    class Program {
-        static void Main(string[] args) {
+    class Program
+    {
+        static void Main(string[] args)
+        {
             EventPublisher obj = new EventPublisher();
             // TODO: use a named function as an event handler
-
+            // obj.ValueChanged += new MyEventHandler(ObjValueChanged);
             // TODO: use an anonymous delegate as an event handler
-
+            obj.ValueChanged += delegate (string val)
+            {
+                Console.WriteLine($"The Value changed to {val}");
+            };
 
             string? str = "";
-            do {
+            do
+            {
                 Console.WriteLine("Enter a value: ");
                 str = Console.ReadLine();
-                if (!str!.Equals("exit")) {
+                if (!str!.Equals("exit"))
+                {
                     obj.Val = str;
                 }
             } while (!str.Equals("exit"));
@@ -40,7 +50,8 @@ namespace BasicEvents
         }
 
         // This function will be called when the value changes in the EventPublisher class
-        static void ObjValueChanged(string value) {
+        static void ObjValueChanged(string value)
+        {
             Console.WriteLine("The value changed to {0}", value);
         }
     }
--- Start/Lambdas/LambdaChallenge/Program.cs
+++ Start/Lambdas/LambdaChallenge/Program.cs
@@ -5,31 +5,39 @@ namespace EventsSolution
 {
     public delegate void BalanceEventHandler(decimal OldValue, decimal NewValue);
 
-    class PiggyBank {
+    class PiggyBank
+    {
         private decimal _BalanceAmount;
         public event BalanceEventHandler? BalanceChanged;
 
-        public decimal TheBalance {
-            set {
+        public decimal TheBalance
+        {
+            set
+            {
                 decimal oldVal = _BalanceAmount;
                 _BalanceAmount = value;
                 if (BalanceChanged is not null) { BalanceChanged(oldVal, _BalanceAmount); }
             }
-            get {
+            get
+            {
                 return _BalanceAmount;
             }
         }
     }
 
-    class Program {
-        static void Main(string[] args) {
+    class Program
+    {
+        static void Main(string[] args)
+        {
             decimal[] testValues = { 250, 1000, -750, 100, -200 };
-            
+
             PiggyBank pb = new PiggyBank();
 
             // TODO: Use a lambda function to implement the event handler
+            pb.BalanceChanged += (x, y) => Console.WriteLine($"The balance changed from {x} to {y}");
 
-            foreach (decimal testValue in testValues) {
+            foreach (decimal testValue in testValues)
+            {
                 pb.TheBalance += testValue;
             }
             Console.WriteLine($"Final value is {pb.TheBalance}");
